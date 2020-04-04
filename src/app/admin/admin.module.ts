a8c1f14368c060b95a1f7478cae033048935aa92
dhrumintechhive <62594521+dhrumintechhive@users.noreply.github.com>
2020-04-04 22:50:19 +0530

    Admin routing completed
    Admin routing completed

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin.component';
+import { ScrumboardComponent } from './scrumboard/scrumboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'scrumboard', component: ScrumboardComponent },
 ];
 
 @NgModule({
-  declarations: [DashboardComponent, AdminComponent],
+  declarations: [DashboardComponent, AdminComponent, ScrumboardComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
