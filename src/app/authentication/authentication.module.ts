f3d90039762230b5ed53cbe13956622361bfb8d2
dhrumintechhive <62594521+dhrumintechhive@users.noreply.github.com>
2020-04-04 22:45:44 +0530

    Authentication routing and Angular Material Added
    Authentication routing and Angular Material Added

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationComponent } from './authentication.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthenticationComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ForgotPasswordComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AuthenticationModule { }
