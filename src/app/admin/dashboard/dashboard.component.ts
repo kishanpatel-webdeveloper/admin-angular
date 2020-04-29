72e18216d288da6c118483c37d1f34b84f78c503
dhrumin16ranoliya <dhrumin16ranoliya@gmail.com>
2020-04-29 22:31:52 +0530

    data-theme attribute dynamically setted in body
    data-theme attribute dynamically setted in body

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {ThemePalette} from '@angular/material/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
+   constructor(@Inject(DOCUMENT) document, r: Renderer2) {
+    }
 
   ngOnInit() {
+  }
+
+  changeTheme(){
+    this.checked = !this.checked;
+    if(this.checked === false){
+     document.body.setAttribute('data-theme', 'light');
+    } else if(this.checked === true){
+     document.body.setAttribute('data-theme', 'dark');
+    }
+
+
   }
 
 }
